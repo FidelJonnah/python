51b1db5b2eecd7f6e091bf2949fe2114997e0544
FidelJonnah <122522680+FidelJonnah@users.noreply.github.com>
2023-01-12 12:29:11 +0300

    Create randomjoke.py

---
+++ beginner projects/Random Joke/randomjoke.py
@@ -0,0 +1,11 @@
+import random
+
+jokes = [
+    "Why did the tomato turn red? Because it saw the salad dressing!",
+    "Why was the math book sad? Because it had too many problems.",
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "Why don't oysters give to charity? Because they're shellfish!",
+    "Why did the cookie go to the doctor? Because it was feeling crumbly!"
+]
+
+print(random.choice(jokes))
